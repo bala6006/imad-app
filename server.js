b6f4e6f5d0bb4c0d1e965447bf9d7594528b4262
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-09 23:04:10 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,6 +13,23 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
+app.get('/test1', function (req,res){
+    res.send('this is test one');
+});
+
+app.get('/test2',function(res,req){
+    res.send('this is test two');
+});
+
+app.get(
+    
+    'test3',function(req,res)
+    {
+        res.send('this is test three');
+    }
+    
+    );
+
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
 });
