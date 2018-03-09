d355f160767931a5d16e154488891d8b90a7ebd7
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-09 23:42:51 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/ui/tes1', function (req,res){
+app.get('tes1', function (req,res){
     res.sendFile(path.join(__dirname,'ui','tes1.html'));
 });
 
