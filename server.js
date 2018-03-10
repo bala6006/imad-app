4f133877794efc4eacf3ffe21fa1e9f0b79075e7
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-10 23:06:56 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -13,8 +13,68 @@ app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
 
-app.get('/test-two', function (req, res) {
-  res.sendFile(path.join(__dirname, 'ui', 'test-two.html'));
+
+var testone={
+    title: 'THIS IS TEST ONE',
+    head: 'TEST-ONE',
+    date: '10-MAR-2018',
+    content: `
+    <div>
+    Here we going to executed test one document
+    
+    
+    </div>
+    `
+    
+};
+
+
+
+function test(data){
+    var title=data.title;
+    var head=data.head;
+    var date=data.date;
+    var content=data.date;
+var htmltest=`
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>
+            ${title}
+        </title>
+        <meta name="viewport" content="width=device-width, initial-scale=1"/>
+        <link href="/ui/style.css" rel="stylesheet"/>
+    </head>
+    <body>
+        <table border=5% align=center>
+            <tr>
+                <td>
+                    <a href='/'>HOME</a>
+                </td>
+            </tr>
+        </table>
+        <div>
+           <h4>${head}</h4>
+        </div>
+        
+        <div>
+            ${date}
+        </div>&nbsp;
+        <DIV>
+             ${content}
+        </DIV>
+    </body>
+</html>
+`;
+return htmltest;
+}
+
+
+
+
+
+app.get('/testone', function (req, res) {
+  res.send(test(testone));
 });
 
 app.get('/test', function (req, res) {
