e465f4a7b6f23dc6c11e4f93edcf7d2901777c62
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-14 00:23:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -2,4 +2,8 @@ console.log('Loaded!');
 
 var ele=document.getElementById('testing');
 ele.innerHTML='HI BALA';
+var img=document.getElementById('madi');
+img.onclick=function(){
+   img.style.marginleft("100px"); 
+};
 
