24eee16d3dd80ed7483717545d69c06daf7380cb
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-15 22:49:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -28,7 +28,7 @@ var counter=0;
 button.onclick=function(){
     
     counter=counter+1;
-    var span=document.getElementById('count');
+    var span=document.getElementById('one');
     span.innerHTML=counter.toString();
   
 };
