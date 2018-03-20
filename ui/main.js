ab6b67caab2bf91053c8b34b21de77632ebc7c15
bala6006 <31168415+bala6006@users.noreply.github.com>
2018-03-21 00:01:49 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -22,7 +22,7 @@ img.onclick=function(){
   var inter=setInterval(marginRight,100);
 };**/
 
-var button=document.getElementById("counter");
+var button=document.getElementById('counter'); 
 //var counter=0;
 
 button.onclick=function(){
